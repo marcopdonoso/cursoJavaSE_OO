27217f91f887541ef9b308225a681bce9deaec37
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-09 15:38:20 -0400

    Utilizando getters and setters para complementar el encapsulamiento de los atributos de la clase Patient, estableciendo asi metodos de acceso y modificacion de los mismos, con la ventaja de poder incorporar logica de verificacion dentro de estos metodos de get y set

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -10,6 +10,12 @@ public class Main {
         System.out.println(myDoctor.speciality);
 
         Patient patient = new Patient("Daniela Riera", "danirieraparedes@gmail.com");
-        
+
+        patient.setWeight(55);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber());
+
     }
 }
