da6d7571e0a5370722c4982e2e37845f1ba7a319
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-09 15:38:20 -0400

    Utilizando getters and setters para complementar el encapsulamiento de los atributos de la clase Patient, estableciendo asi metodos de acceso y modificacion de los mismos, con la ventaja de poder incorporar logica de verificacion dentro de estos metodos de get y set

--- MyMedicalAppointments/src/Patient.java
+++ MyMedicalAppointments/src/Patient.java
@@ -13,4 +13,80 @@ public class Patient {
         this.name = name;
         this.email = email;
     }
+
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+
+    public String getWeight(){
+        return this.weight + "kg";
+    }
+
+    public String getHeight() {
+        return height + "m";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public System getAddress() {
+        return address;
+    }
+
+    public void setAddress(System address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() > 8){
+            System.out.println("Se ha superado el nro maximo de digitos");
+        } else if (phoneNumber.length() == 8) {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
 }
