274097157f9d9b3aa16e23cbe1df8a896f6c1bf8
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-10 11:06:12 -0400

    Variable vs Objeto: Un vistazo a la memoria, cuando asignamos un objeto a otro objeto, lo que se asigna realmente no son sus atributos y metodos, sino su direccion en la memoria Stack

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -9,13 +9,34 @@ public class Main {
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
 
-        Patient patient = new Patient("Daniela Riera", "danirieraparedes@gmail.com");
+        //Variable vs Objeto: Un vistazo a la memoria
+        int i = 0;
+        int b = 2;
+        b = i;
+
+        System.out.println();
+        System.out.println();
+        Patient patient = new Patient("Sondo", "Gastro");
+        Patient patient2 = new Patient("Druno", "Gineco");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        /*Patient patient = new Patient("Daniela Riera", "danirieraparedes@gmail.com");
 
         patient.setWeight(55);
         System.out.println(patient.getWeight());
 
         patient.setPhoneNumber("12345678");
-        System.out.println(patient.getPhoneNumber());
+        System.out.println(patient.getPhoneNumber());*/
 
     }
 }
