1dee6606c36bd8267dc34c906738cf061165a1f1
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-11 15:42:43 -0400

    Interfaces: creamos la inteface ISchedulable para que el metodo contenido por esta schedule() se obligatorio para las clases donde esta implementada esta interface, en este caso AppointmentDoctor y AppointmentNurse, dichas clases encargadas de agendar citas ya sea a un doctor o una nurse, deben invocar al metodo schedule() de la interface con acciones propias en cada clase invocadora

--- MyMedicalAppointments/src/model/User.java
+++ MyMedicalAppointments/src/model/User.java
@@ -1,3 +1,5 @@
+package model;
+
 public class User {
     private int id;
     private String name;
@@ -56,6 +58,6 @@ public class User {
 
     @Override
     public String toString() {
-        return "User: " + name + ", Email: " + email + "\nAddress: " + address + ". Phone: " + phoneNumber;
+        return "model.User: " + name + ", Email: " + email + "\nAddress: " + address + ". Phone: " + phoneNumber;
     }
 }
